89f59047c526e7e8efd51defeacd9095f3d5b4c5
mindhacker0 <983213779@qq.com>
2021-08-03 18:09:23 +0800

    webgl2

--- src/components/webgl/index.vue
+++ src/components/webgl/index.vue
@@ -17,7 +17,7 @@ export default {
     },
     mounted:function(){
         this.gl = document.getElementById("glTest").getContext("webgl");
-        this.drawPoint();
+        this.drawRect();
     },
     methods:{
         drawPoint(){
@@ -31,10 +31,47 @@ export default {
             //片段着色器
             var fragShaderSource = '' +
             'void main(){' +
-            'gl_FragColor = vec4(1.0,0.0,0.0,1.0);'+
+            'gl_FragColor = vec4(1.0,1.0,0.0,0.5);'+
             '}';
             var program = this.initShader(vertexShaderSource,fragShaderSource);
             gl.drawArrays(gl.POINTS,0,1);
+        },
+        drawRect(){
+            const {gl} = this;
+            //矩形四个顶点坐标的数据
+            // var data = new Float32Array([0.5,0.5,-0.5,0.5,-0.5,-0.5,0.5,-0.5]);
+            // 数组里9个元素，每间隔3个为一组，分别代表zyx轴上的坐标值
+            var data=new Float32Array([
+                0.0, 0.0, 1.0,//三角形顶点1坐标
+                0.0, 0.5, 0.0,//三角形顶点2坐标
+                1.0, 0.0, 0.0//三角形顶点3坐标
+            ]);
+            //顶点着色器
+            var  vertexShaderSource = ''+
+            'attribute vec4 apos;'+
+            'void main(){' +
+                'gl_Position = apos;' +
+            '}';
+            //片段着色器
+            var fragShaderSource = '' +
+            'void main(){' +
+            'gl_FragColor = vec4(1.0,1.0,0.0,0.5);'+
+            '}';
+            var program = this.initShader(vertexShaderSource,fragShaderSource);
+            //获取顶点着色器的位置变量apos，即aposLocation指向apos变量。
+            var aposLocation = gl.getAttribLocation(program,'apos');
+            //创建缓冲区对象
+            var buffer = gl.createBuffer();
+            //绑定缓冲区对象,激活buffer
+            gl.bindBuffer(gl.ARRAY_BUFFER,buffer);
+            //顶点数组传入buffer缓冲区
+            gl.bufferData(gl.ARRAY_BUFFER,data,gl.STATIC_DRAW);
+            //缓冲区中的数据按照一定的规律传递给位置变量apos
+            gl.vertexAttribPointer(aposLocation,3,gl.FLOAT,false,0,0);
+            //允许数据传递
+            gl.enableVertexAttribArray(aposLocation);
+            //开始绘制图形
+            gl.drawArrays(gl.TRIANGLES,0,3);
         },
         initShader(vertexShaderSource,fragShaderSource){
             const {gl} = this;
