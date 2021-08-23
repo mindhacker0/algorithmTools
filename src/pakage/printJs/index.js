600f98d8d29253b295072d563cad535188dc0c74
mindhacker <983213779@qq.com>
2021-08-23 20:18:47 +0800

    add chinese comments

--- src/pakage/printJs/index.js
+++ src/pakage/printJs/index.js
@@ -1,21 +1,21 @@
-function GUUID(prefix){//random key,to generate class names
+function GUUID(prefix){//产生GUUID(一个随机字符串),作为类名
     var s = [];
     var hexDigits = "0123456789abcdef";
     for (var i = 0; i < 36; i++) {
         s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
     }
-    s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
-    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[14] = "4";
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);
     s[8] = s[13] = s[18] = s[23] = "-";
     var uuid = s.join("");
     return prefix+uuid;
 }
-const standards = {
+const standards = {//文档类型
     strict: 'strict',
     loose: 'loose',
     html5: 'html5'
 };
-class DomNode{//dom node
+class DomNode{//dom 节点类
     constructor(element){
         if(!element instanceof HTMLElement) return new Error(element+"is not a HTMLElement");
         this.style = this.getStaticStyle(element);
@@ -25,7 +25,7 @@ class DomNode{//dom node
         this.standard = standards.html5;
         this.childNodes = [];
     }
-    getStaticStyle(element){//compute element styles
+    getStaticStyle(element){//计算元素的样式，存储起来以生成样式列表。
        const allStyles =  window.getComputedStyle(element);
        const style = new Map();
        for(var i in allStyles){
@@ -36,7 +36,7 @@ class DomNode{//dom node
        return style;
     }
 }
-class textNode{//text node
+class textNode{//文本节点
     constructor(element){
         this.content = element.textContent;
     }
@@ -55,10 +55,10 @@ class Print{
         this.body = this.getBody();
         this.makeFrame();
     }
-    makeCloneDomWithStyle(){//拷贝所有需要克隆的元素的样式
+    makeCloneDomWithStyle(){//拷贝所有需要克隆的元素的样式生并成dom树
         const vm =this;
-        function copyELements(parent){//tranvers all elements and resolve their styles inoder to copy to new frame.
-            if(parent instanceof Element){//plain html elements
+        function copyELements(parent){//遍历dom树，解析它们的当前样式。
+            if(parent instanceof Element){//元素节点
                 const dom = new DomNode(parent);
                 vm.styleSheets.set(dom.className,dom.style);
                 let children = parent.childNodes;
@@ -68,16 +68,16 @@ class Print{
                     }
                 }
                 return dom;
-            }else if(parent instanceof Text){//text nodes
+            }else if(parent instanceof Text){//文本节点
                return new textNode(parent);
             }
             //console.log(parent.__proto__,parent);
-            //comments or other invalid token
+            //包含注释和其他的字符被过滤掉，如果需要后面可以处理
             return parent;
         }
-        this.domTree = copyELements(this.printDom);//ananlyse dom struct and trans into Object.
+        this.domTree = copyELements(this.printDom);
     }
-    makeFrame(){//make iframe
+    makeFrame(){//构造iframe,使我们构造的页面放进去调用window.print()打印。
         let iframe = document.createElement('iframe');
         iframe.style.border = '0px';
         iframe.style.position = 'absolute';
@@ -96,7 +96,7 @@ class Print{
         }
         this.frame = iframe;
     }
-    getDocType(){//make doctype,how to decode this document
+    getDocType(){//构造文档声明，如何解析文档
         if (this.standard === standards.html5) {
             return '<!DOCTYPE html>';
         }
@@ -104,11 +104,11 @@ class Print{
         var dtd = this.standard === standards.loose ? 'loose' : 'strict';
         return `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01${transitional}//EN" "http://www.w3.org/TR/html4/${dtd}.dtd">`;
     }
-    getHead() {
+    getHead() {//构造头部，样式也将构造存放在这儿
         let links = '';
         let style = '';
         // 复制所有link标签
-        [].forEach.call(document.querySelectorAll('link'), function (item) {
+        [].forEach.call(document.querySelectorAll('link'), function(item) {
           if(item.href.indexOf('.css') >= 0){
             links += `<link type="text/css" rel="stylesheet" href="${item.href}" >`;
           }
@@ -122,10 +122,10 @@ class Print{
         });
         return `<head><title>${this.title}</title>${links}<style type="text/css">${style}</style></head>`;
     }
-    getBody(){
+    getBody(){//构造body，遍历之前创建的Dom树并添加样式，存放在fragment中
         let wrapper = document.createDocumentFragment();
         function renderElement(vNode,root){
-            if(vNode instanceof DomNode){
+            if(vNode instanceof DomNode){//还原元素节点
                 let elem = document.createElement(vNode.nodeName);
                 elem.classList.add(vNode.className);
                 const children = vNode.childNodes;
@@ -135,7 +135,7 @@ class Print{
                     }
                 }
                 root.appendChild(elem);
-            }else if(vNode instanceof textNode){//text nodes
+            }else if(vNode instanceof textNode){//还原文本节点
                 root.appendChild(document.createTextNode(vNode.content));
             }
         }
